ddd1c8e75fa2e52f04101a5688ca9f9abe7979e0
Neeiyo <louis.grandjean99@gmail.com>
2021-01-26 11:51:42 +0100

    player and app

--- app.rb
+++ app.rb
@@ -3,3 +3,32 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
+
+
+
+#player1.show_state
+#binding.pry
+
+def perform
+	player1 = Player.new("Josiane")
+	player2 = Player.new("Josè")
+	puts "Voici l'état de chaque joueurs: "
+	#puts " "
+	puts "#{player1.show_state} #{player2.show_state}"
+	while player1.life_points > 0 && player2.life_points > 0
+		if !(player1.life_points <= 0)
+			player1.attacks(player2)
+			puts " "
+		else
+			return "#{player1} est mort"
+		end
+		if !(player2.life_points <= 0)
+			player2.attacks(player1)
+			puts " "
+		else
+			return "#{player2} est mort"
+		end
+	end
+end
+
+perform
--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,41 @@
 require 'pry'
+class Player
+    attr_accessor :name, :life_points
+    @@all_players = Array.new
+
+    def initialize (name)
+				@name = name
+				@life_points = 10
+				@@all_players << self
+		end
+
+		def show_state
+			puts "#{@name} a #{@life_points} points de vie."
+		end
+
+		def gets_damage(damage)
+			@life_points = @life_points - damage
+			if @life_points < 1
+				puts "le joueur #{@name} a été tué !"
+			end
+			return @life_points
+		end
+
+		def attacks(player)
+			puts "le joueur #{@name} attaque le joueur #{player.name}"
+			damage_taken = compute_damage
+			puts "il lui inflige #{damage_taken} points de dommages"
+		 player.gets_damage(damage_taken)
+		 player.show_state
+
+		end
+
+		def compute_damage
+			return rand(1..6)
+		end
+
+		def self.all 
+			return @@all_players
+		end
+
+	end
