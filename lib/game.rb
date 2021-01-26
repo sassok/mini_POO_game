f48fbdfb451cd54944f23a77dba652649b286fd8
anthopayan <anthony.payan08@outlook.fr>
2021-01-26 17:49:55 +0100

    en_cours

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,13 @@
 require 'pry'
+
+class Game
+attr_accessor :human_player, :enemies
+
+@enemies = []
+
+def initialize(name_human)
+@human_player = HumanPlayer.new(name_human)
+@enemies = ["Josiane","Jean","Paul","Hanz"]
+
+end
+end
