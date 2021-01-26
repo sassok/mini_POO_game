d68d3fa658dd4c5146e7bdaabe11202670593c31
volpito <mpj.rigot@gmail.com>
2021-01-26 13:35:02 +0100

    la V2 jolie

--- app_2.rb
+++ app_2.rb
@@ -5,8 +5,13 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 	def perform
-		puts "Bonjour, et bienvenue dans la V1 de notre jeu de baston interractif !"
+		puts "  Bonjour, et bienvenue dans la V1 de notre jeu de baston interactif :"
+		puts ""
+		puts "‿︵‿︵(ಥ﹏ಥ)‿︵‿︵     ILS VEULENT TOUS MA POO !!    ‿︵‿︵(ಥ﹏ಥ)‿︵‿︵"
+		puts "            ●○●○●○●○●○●○●○●○●○ （´д｀）●○●○●○●○●○●○●○●○●○"
+		puts ""
 		puts "DIS TON NOMS MATELOT"
+		puts ""
 		print "> "
 		player1 = HumanPlayer.new("#{gets.chomp.to_s}")
 		print "Bienvenue #{player1.name} !"
